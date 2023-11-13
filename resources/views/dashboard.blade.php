2b7826553a37fc1e06dbef72ed0fe72ab7cffba9
Dawid "makaroni' Markieton <57560419+greenMakaroni@users.noreply.github.com>
2023-11-13 18:04:04 +0000

    create layout, render content section in a layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Dashboard </title>
-</head>
-<body>
+@extends('layout')
+
+@section('content')
     <h1>
         {{$heading}}
     </h1>
@@ -19,5 +14,4 @@
             <p> {{$note['content']}} </p>
         @endforeach
 
-</body>
-</html>
+@endsection
--- resources/views/note.blade.php
+++ resources/views/note.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title> {{$note['title']}} </title>
-</head>
-<body>
+@extends('layout')
+
+@section('note')
     <h1> {{$note['title']}} </h1>
     <p> {{$note['content']}} </p>
-</body>
-</html>
+@endsection
